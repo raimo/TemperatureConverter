9f13d4e6907d12985c7aba48bf69ba534ae6384e
Raimo Tuisku <raimo.tuisku@gmail.com>
2013-10-06 15:44:32 -0700

    Add some text boxes to finish the temperature converter.

--- TemperatureConverter/ViewController.m
+++ TemperatureConverter/ViewController.m
@@ -9,6 +9,14 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *celcius;
+@property (weak, nonatomic) IBOutlet UITextField *fahrenheit;
+- (IBAction)convertPressed:(id)sender;
+- (IBAction)onTap:(id)sender;
+- (IBAction)celciusChanged:(id)sender;
+- (IBAction)fahrenheitChanged:(id)sender;
+- (IBAction)fahrenheitTapped:(id)sender;
+- (IBAction)celciusTapped:(id)sender;
 
 @end
 
@@ -17,7 +25,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    self.title = @"Raimo's Temperature Converter";
 }
 
 - (void)didReceiveMemoryWarning
@@ -26,4 +34,39 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)convertPressed:(id)sender {
+    if ([self.celcius.text length] == 0) {
+        float value = ([self.fahrenheit.text floatValue] - 32) * 5.0 / 9;
+        self.celcius.text = [NSString stringWithFormat:@"%0.2f", value];
+    } else if ([self.fahrenheit.text length] == 0) {
+        float value = [self.celcius.text floatValue] * 9.0/5 + 32;
+        self.fahrenheit.text = [NSString stringWithFormat:@"%0.2f", value];
+
+    }
+}
+- (IBAction)onTap:(id)sender {
+    [self.view endEditing:YES];
+}
+
+- (IBAction)celciusChanged:(id)sender {
+    self.fahrenheit.text = @"";
+    [self convertPressed:sender];
+}
+
+- (IBAction)fahrenheitChanged:(id)sender {
+    self.celcius.text = @"";
+    [self convertPressed:sender];
+}
+
+- (IBAction)fahrenheitTapped:(id)sender {
+    if ([self.fahrenheit.text isEqualToString:@"Fahrenheit"]) {
+        self.fahrenheit.text = @"";
+    }
+}
+
+- (IBAction)celciusTapped:(id)sender {
+    if ([self.celcius.text isEqualToString:@"Celcius"]) {
+        self.celcius.text = @"";
+    }
+}
 @end
