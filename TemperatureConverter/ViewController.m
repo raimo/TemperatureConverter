2fa4a6ae9cc21c6597b3c0eb9a1d6263db6c2027
Raimo Tuisku <raimo.tuisku@gmail.com>
2013-10-14 10:39:33 -0700

    Use UITextFieldDelegate instead of wiring UITextField events individually.

--- TemperatureConverter/ViewController.m
+++ TemperatureConverter/ViewController.m
@@ -13,10 +13,6 @@
 @property (weak, nonatomic) IBOutlet UITextField *fahrenheit;
 - (IBAction)doConversion:(id)sender;
 - (IBAction)onTap:(id)sender;
-- (IBAction)celsiusChanged:(id)sender;
-- (IBAction)fahrenheitChanged:(id)sender;
-- (IBAction)fahrenheitTapped:(id)sender;
-- (IBAction)celsiusTapped:(id)sender;
 
 @end
 
@@ -28,6 +24,8 @@ NSString *const FAHRENHEIT_LABEL = @"Fahrenheit";
 {
     [super viewDidLoad];
     self.title = @"Raimo's Temperature Converter";
+    self.fahrenheit.delegate = self;
+    self.celsius.delegate = self;
 }
 
 - (void)didReceiveMemoryWarning
@@ -47,32 +45,28 @@ NSString *const FAHRENHEIT_LABEL = @"Fahrenheit";
     } else if ([self.fahrenheit.text length] == 0) {
         float value = [self.celsius.text floatValue] * 9.0/5 + 32;
         self.fahrenheit.text = [NSString stringWithFormat:@"%0.2f", value];
-
     }
 }
 - (IBAction)onTap:(id)sender {
     [self.view endEditing:YES];
 }
 
-- (IBAction)celsiusChanged:(id)sender {
-    self.fahrenheit.text = @"";
-    [self doConversion:sender];
-}
-
-- (IBAction)fahrenheitChanged:(id)sender {
-    self.celsius.text = @"";
-    [self doConversion:sender];
-}
-
-- (IBAction)fahrenheitTapped:(id)sender {
-    if ([self.fahrenheit.text isEqualToString:FAHRENHEIT_LABEL]) {
+- (void)textFieldDidEndEditing:(UITextField *)sender {
+    if (sender == self.celsius) {
         self.fahrenheit.text = @"";
-    }
-}
-
-- (IBAction)celsiusTapped:(id)sender {
-    if ([self.celsius.text isEqualToString:CELSIUS_LABEL]) {
+    } else if (sender == self.fahrenheit) {
         self.celsius.text = @"";
     }
+    [self doConversion:sender];
 }
+
+- (void)textFieldDidBeginEditing:(UITextField *)sender {
+    if (sender == self.fahrenheit && [self.fahrenheit.text isEqualToString:FAHRENHEIT_LABEL]) {
+        self.fahrenheit.text = @"";
+    } else if (sender == self.celsius && [self.celsius.text isEqualToString:CELSIUS_LABEL]) {
+        self.celsius.text = @"";
+    }
+
+}
+
 @end
